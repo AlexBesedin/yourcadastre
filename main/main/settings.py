af5c35ea385590303bcc6447a0692b39d4cd8408
AlexBesedin <alebesedin@yandex.ru>
2023-11-17 19:53:34 +0200

    fix docker-compose, fix tasks, add postgresql

--- main/main/settings.py
+++ main/main/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
--- main/service/tasks.py
+++ main/service/tasks.py
@@ -1,15 +1,13 @@
-# tasks.py
-
+import time  
+import random
 from celery import shared_task
-import time  # Для эмуляции задержки
-import random  # Для генерации случайного ответа
+
 
 @shared_task
 def process_query(query_id):
-    time.sleep(random.randint(0, 60))  # Симуляция задержки до 60 секунд
-    response = random.choice([True, False])  # Случайный ответ True или False
-
-    # Найти и обновить соответствующий QueryHistory объект
+    time.sleep(random.randint(0, 60)) 
+    response = random.choice([True, False])
+    
     from service.models import QueryHistory
     query_instance = QueryHistory.objects.get(id=query_id)
     query_instance.response = response
